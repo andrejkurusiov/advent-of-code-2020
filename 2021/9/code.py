5ae0a837ba030c435912bc36c79e5b22b5600880
Andrej Kurusiov <38130665+andrejkurusiov@users.noreply.github.com>
2022-01-08 20:38:04 +0200

    day 9 part 2

--- 2021/9/code.py
+++ 2021/9/code.py
@@ -37,18 +37,20 @@ def min_of_four(field: list[list[int]], i: int, j: int) -> int:
     return min(comp_lst)
 
 
-def part_1(field: list[list[int]]) -> int:
+def part_1(field: list[list[int]]) -> tuple[int, list[tuple[int, int, int]]]:
     """ The risk level of a low point is 1 plus its height.
         What is the sum of the risk levels of all low points on your heightmap? """
     rows = len(field)
     columns = len(field[0])
+    low_points = []
     risk_lvl = 0
     for i in range(rows):
         for j in range(columns):
             tst = field[i][j]
             if tst < min_of_four(field, i, j):
                 risk_lvl += tst + 1
-    return risk_lvl
+                low_points.append((i, j, tst))
+    return (risk_lvl, low_points)
 
 
 TEST_DATA = '''2199943210
@@ -60,8 +62,47 @@ TEST_DATA = '''2199943210
 # Your first goal is to find the low points - the locations that are lower than any of its adjacent locations. Most locations have four adjacent locations (up, down, left, and right); locations on the edge or corner of the map have three or two adjacent locations, respectively. (Diagonal locations do not count as adjacent.)
 
 
-def part_2(input_data: str) -> int:
-    pass
+def explore_basin(field: list[list[int]], i: int, j: int, visited_nodes: list[tuple[int, int]], i_max: int, j_max: int) -> list:
+    """Recurses to explore and form basins.
+        Args:
+            field (list[list[int]]): initial array of depths
+            i (int): row
+            j (int): column
+            visited_nodes (list[tuple[int, int]]): list of already visited nodes
+            i_max (int): max index of row
+            j_max (int): max index of column
+
+        Returns:
+            list: visited_nodes[] list of coordinates (tuple[int, int])
+    """
+    # node outside of a min/max index, of value 9, or was visited --> exit recursion
+    if (i in range(0, i_max) and j in range(0, j_max) and field[i][j] != 9 and (i, j) not in visited_nodes):
+        visited_nodes.append((i, j))
+        # check nodes around the current one
+        explore_basin(field, i+1, j, visited_nodes, i_max, j_max)
+        explore_basin(field, i-1, j, visited_nodes, i_max, j_max)
+        explore_basin(field, i, j+1, visited_nodes, i_max, j_max)
+        explore_basin(field, i, j-1, visited_nodes, i_max, j_max)
+
+        return visited_nodes
+
+
+def part_2(field: list[list[int]]) -> int:
+    """ What do you get if you multiply together the sizes of the three largest basins?
+        Uses already identified low points from Part_1 and explores basins around them. """
+    basins = []  # list of basin lengths (int)
+    i_max = len(field)
+    j_max = len(field[0])
+    low_points = part_1(field)[1]
+
+    # for each low point explore its basin and add its length to basins[]
+    for i, j, _ in low_points:
+        visited = []
+        basins.append(len(explore_basin(
+            field, i, j, visited, i_max, j_max)))
+
+    basins.sort(reverse=True)
+    return (basins[0] * basins[1] * basins[2])
 
 
 # --- MAIN ---
@@ -69,5 +110,5 @@ if __name__ == "__main__":
     in_data = read_file()
     # in_data = TEST_DATA
     in_data = parse_input(in_data)
-    print("Part One : " + str(part_1(in_data)))
+    print("Part One : " + str(part_1(in_data)[0]))
     print("Part Two : " + str(part_2(in_data)))
