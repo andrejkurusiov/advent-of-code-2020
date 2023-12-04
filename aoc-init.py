2c659689147dfc173c210f38d796e2272ef40282
Andrej Kurusiov <38130665+andrejkurusiov@users.noreply.github.com>
2023-12-04 13:30:03 +0200

    formatting

--- aoc-init.py
+++ aoc-init.py
@@ -21,15 +21,15 @@ import os
 # import datetime
 
 
-# config = {"USER": "foo", "EMAIL": "foo@example.org"}
-config = dotenv_values(".env")
+# config = {'USER': 'foo', 'EMAIL': 'foo@example.org'}
+config = dotenv_values('.env')
 
-base_pos = "./"
-# Get your session by inspecting the session cookie content in your web browser while connected to adventofcode and paste it here as plain text in between the ". Leave at is to not download inputs.
-USER_SESSION_ID = config["USER_SESSION_ID"]
+base_pos = './'
+# Get your session by inspecting the session cookie content in your web browser while connected to adventofcode and paste it here as plain text in between the '. Leave at is to not download inputs.
+USER_SESSION_ID = config['USER_SESSION_ID']
 
 # Name automatically put in the code templates.
-AUTHOR = config["AUTHOR"]
+AUTHOR = config['AUTHOR']
 
 # Set to false to not download statements. Note that only part one is downloaded (since you need to complete it to access part two)
 DOWNLOAD_STATEMENTS = True
@@ -44,14 +44,14 @@ MAKE_CODE_TEMPLATE = True
 MAKE_URL = True
 
 # If you really need to download the whole thing again, set this to true. As the creator said, AoC is fragile; please be gentle. Statements and Inputs do not change. This will not overwrite codes.
-OVERWRITE = config["OVERWRITE"]
+OVERWRITE = config['OVERWRITE']
 
 
 # Code template
 CODE_TEMPLATE_TEXT = """
 
 def read_file() -> str:
-    with open((__file__.rstrip("code.py")+"input.txt"), 'r', encoding="utf-8") as input_file:
+    with open((__file__.rstrip('code.py')+'input.txt'), 'r', encoding='utf-8') as input_file:
         input_data = input_file.read()
     return input_data
 
@@ -76,23 +76,23 @@ def part_2(data: list[str]) -> int:
 
 
 # --- MAIN ---
-if __name__ == "__main__":
+if __name__ == '__main__':
     in_data = read_file()
     in_data = TEST_DATA     # comment out to use real data
     in_data = parse_input(in_data)
-    print("Part One : "+ str(part_1(in_data)))
-    print("Part Two : "+ str(part_2(in_data)))"""
+    print('Part One : '+ str(part_1(in_data)))
+    print('Part Two : '+ str(part_2(in_data)))"""
 
 # DATE SPECIFIC PARAMETERS
 # Date text automatically put in the code templates.
-date = config["DATE"]
+date = config['DATE']
 starting_advent_of_code_year = int(
-    config["STARTING_ADVENT_OF_CODE_YEAR"]
+    config['STARTING_ADVENT_OF_CODE_YEAR']
 )  # You can go as early as 2015.
 # The setup will download all advent of code data up until that date included
-last_advent_of_code_year = int(config["LAST_ADVENT_OF_CODE_YEAR"])
+last_advent_of_code_year = int(config['LAST_ADVENT_OF_CODE_YEAR'])
 # If the year isn't finished, the setup will download days up until that day included for the last year
-last_advent_of_code_day = int(config["LAST_ADVENT_OF_CODE_DAY"])
+last_advent_of_code_day = int(config['LAST_ADVENT_OF_CODE_DAY'])
 
 
 # Code
@@ -101,17 +101,17 @@ years = range(starting_advent_of_code_year, last_advent_of_code_year + 1)
 days = range(1, 26)
 
 # ex use : https://adventofcode.com/2017/day/19/input
-link = "https://adventofcode.com/"
+link = 'https://adventofcode.com/'
 
-USER_AGENT = "adventofcode_working_directories_creator"
+USER_AGENT = 'adventofcode_working_directories_creator'
 
 print(
-    "Setup will download data and create working directories and files for adventofcode."
+    'Setup will download data and create working directories and files for adventofcode.'
 )
 if not os.path.exists(base_pos):
     os.mkdir(base_pos)
 for y in years:
-    print("Year " + str(y))
+    print('Year ' + str(y))
     if not os.path.exists(base_pos + str(y)):
         os.mkdir(base_pos + str(y))
     year_pos = base_pos + str(y)
@@ -120,20 +120,20 @@ for y in years:
         for d in days
         if (y < last_advent_of_code_year or d <= last_advent_of_code_day)
     ):
-        print("    Day " + str(d))
-        if not os.path.exists(year_pos + "/" + str(d)):
-            os.mkdir(year_pos + "/" + str(d))
-        day_pos = year_pos + "/" + str(d)
-        if MAKE_CODE_TEMPLATE and not os.path.exists(day_pos + "/code.py"):
-            code = open(day_pos + "/code.py", "w+")
+        print('    Day ' + str(d))
+        if not os.path.exists(year_pos + '/' + str(d)):
+            os.mkdir(year_pos + '/' + str(d))
+        day_pos = year_pos + '/' + str(d)
+        if MAKE_CODE_TEMPLATE and not os.path.exists(day_pos + '/code.py'):
+            code = open(day_pos + '/code.py', 'w+')
             code.write(
-                "# Advent of code Year "
+                '# Advent of code Year '
                 + str(y)
-                + " Day "
+                + ' Day '
                 + str(d)
-                + " solution\n# Author = "
+                + ' solution\n# Author = '
                 + AUTHOR
-                + "\n# Date = "
+                + '\n# Date = '
                 + date
                 + CODE_TEMPLATE_TEXT
             )
@@ -141,62 +141,62 @@ for y in years:
             code.close()
         if (
             DOWNLOAD_INPUTS
-            and (not os.path.exists(day_pos + "/input.txt") or OVERWRITE)
-            and USER_SESSION_ID != ""
+            and (not os.path.exists(day_pos + '/input.txt') or OVERWRITE)
+            and USER_SESSION_ID != ''
         ):
             done = False
             error_count = 0
             while not done:
                 try:
                     with requests.get(
-                        url=link + str(y) + "/day/" + str(d) + "/input",
-                        cookies={"session": USER_SESSION_ID},
-                        headers={"User-Agent": USER_AGENT},
+                        url=link + str(y) + '/day/' + str(d) + '/input',
+                        cookies={'session': USER_SESSION_ID},
+                        headers={'User-Agent': USER_AGENT},
                     ) as response:
                         if response.ok:
                             data = response.text
-                            input = open(day_pos + "/input.txt", "w+")
-                            input.write(data.rstrip("\n"))
+                            input = open(day_pos + '/input.txt', 'w+')
+                            input.write(data.rstrip('\n'))
                             input.close()
                         else:
-                            print("        Server response for input is not valid.")
+                            print('        Server response for input is not valid.')
                     done = True
                 except requests.exceptions.RequestException:
                     error_count += 1
                     if error_count > MAX_RECONNECT_ATTEMPT:
-                        print("        Giving up.")
+                        print('        Giving up.')
                         done = True
                     elif error_count == 0:
                         print(
-                            "        Error while requesting input from server. Request probably timed out. Trying again."
+                            '        Error while requesting input from server. Request probably timed out. Trying again.'
                         )
                     else:
-                        print("        Trying again.")
+                        print('        Trying again.')
                 except Exception as e:
                     print(
-                        "        Non handled error while requesting input from server. "
+                        '        Non handled error while requesting input from server. '
                         + str(e)
                     )
                     done = True
         if DOWNLOAD_STATEMENTS and (
-            not os.path.exists(day_pos + "/statement.html") or OVERWRITE
+            not os.path.exists(day_pos + '/statement.html') or OVERWRITE
         ):
             done = False
             error_count = 0
             while not done:
                 try:
                     with requests.get(
-                        url=link + str(y) + "/day/" + str(d),
-                        cookies={"session": USER_SESSION_ID},
-                        headers={"User-Agent": USER_AGENT},
+                        url=link + str(y) + '/day/' + str(d),
+                        cookies={'session': USER_SESSION_ID},
+                        headers={'User-Agent': USER_AGENT},
                     ) as response:
                         if response.ok:
                             html = response.text
-                            start = html.find("<article")
-                            end = html.rfind("</article>") + len("</article>")
+                            start = html.find('<article')
+                            end = html.rfind('</article>') + len('</article>')
                             end_success = html.rfind(
-                                "</code>") + len("</code>")
-                            statement = open(day_pos + "/statement.html", "w+")
+                                '</code>') + len('</code>')
+                            statement = open(day_pos + '/statement.html', 'w+')
                             statement.write(html[start: max(end, end_success)])
                             statement.close()
                         done = True
@@ -204,26 +204,26 @@ for y in years:
                     error_count += 1
                     if error_count > MAX_RECONNECT_ATTEMPT:
                         print(
-                            "        Error while requesting statement from server. Request probably timed out. Giving up."
+                            '        Error while requesting statement from server. Request probably timed out. Giving up.'
                         )
                         done = True
                     else:
                         print(
-                            "        Error while requesting statement from server. Request probably timed out. Trying again."
+                            '        Error while requesting statement from server. Request probably timed out. Trying again.'
                         )
                 except Exception as e:
                     print(
-                        "        Non handled error while requesting statement from server. "
+                        '        Non handled error while requesting statement from server. '
                         + str(e)
                     )
                     done = True
-        if MAKE_URL and (not os.path.exists(day_pos + "/link.url") or OVERWRITE):
-            url = open(day_pos + "/link.url", "w+")
+        if MAKE_URL and (not os.path.exists(day_pos + '/link.url') or OVERWRITE):
+            url = open(day_pos + '/link.url', 'w+')
             url.write(
-                "[InternetShortcut]\nURL=" + link +
-                str(y) + "/day/" + str(d) + "\n"
+                '[InternetShortcut]\nURL=' + link +
+                str(y) + '/day/' + str(d) + '\n'
             )
             url.close()
 print(
-    "Setup complete : adventofcode working directories and files initialized with success."
+    'Setup complete : adventofcode working directories and files initialized with success.'
 )
